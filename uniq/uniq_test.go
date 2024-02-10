bbfdce4630991cfa472563d88e283642fc5972a7
Bopster411 <serg.dr2004@gmail.com>
2024-02-10 19:32:37 +0300

    Added require package and rewrite tests asserts

--- uniq/uniq_test.go
+++ uniq/uniq_test.go
@@ -2,33 +2,29 @@ package uniq
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestNoDuplicate(t *testing.T) {
 	input := "hello world!"
 	output := uniq(input)
 	correctOutput := "hello world!"
-	if output != correctOutput {
-		t.Fatalf("Strings don't match!\n(correct is '%v', yours is '%v'", correctOutput, output)
-	}
+	require.Equalf(t, correctOutput, output, "Strings don't match!\n(correct is '%v', yours is '%v'", correctOutput, output)
 }
 
 func TestOneDuplicate(t *testing.T) {
 	input := "hello world!\nhello world!"
 	output := uniq(input)
 	correctOutput := "hello world!"
-	if output != correctOutput {
-		t.Fatalf("Strings don't match!\n(correct is '%v', yours is '%v'", correctOutput, output)
-	}
+	require.Equalf(t, correctOutput, output, "Strings don't match!\n(correct is '%v', yours is '%v'", correctOutput, output)
 }
 
 func TestDifferentLines(t *testing.T) {
 	input := "hello world!\nhello world!\nhi"
 	output := uniq(input)
 	correctOutput := "hello world!\nhi"
-	if output != correctOutput {
-		t.Fatalf("Strings don't match!\n(correct is '%v', yours is '%v'", correctOutput, output)
-	}
+	require.Equalf(t, correctOutput, output, "Strings don't match!\n(correct is '%v', yours is '%v'", correctOutput, output)
 }
 
 func TestDeleteIfNear(t *testing.T) {
@@ -40,7 +36,22 @@ hi`
 	correctOutput := `hi
 hello world!
 hi`
-	if output != correctOutput {
-		t.Fatalf("Strings don't match!\n(correct is '%v', yours is '%v'", correctOutput, output)
-	}
+	require.Equalf(t, correctOutput, output, "Strings don't match!\n(correct is '%v', yours is '%v'", correctOutput, output)
+}
+
+func TestEmptyLines(t *testing.T) {
+	input := `hi
+
+hello world!
+hello world!
+
+
+hi`
+	output := uniq(input)
+	correctOutput := `hi
+
+hello world!
+
+hi`
+	require.Equalf(t, correctOutput, output, "Strings don't match!\n(correct is '%v', yours is '%v'", correctOutput, output)
 }
