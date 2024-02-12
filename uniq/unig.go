7df38626063a6561dc512264ea7d46e64acb96f3
Bopster411 <serg.dr2004@gmail.com>
2024-02-12 11:05:32 +0300

    Added count option

--- uniq/unig.go
+++ uniq/unig.go
@@ -2,6 +2,7 @@ package uniq
 
 import (
 	"bufio"
+	"fmt"
 	"strings"
 )
 
@@ -16,13 +17,30 @@ type Options struct {
 
 func uniq(input []string, options Options) string {
 	var outputSlice []string
-	var workingLine string = ""
+	var prevLine string = input[0]
+	var counter int = 0
+
 	for _, currentLine := range input {
-		if currentLine != workingLine {
-			outputSlice = append(outputSlice, currentLine)
-			workingLine = currentLine
+		if currentLine != prevLine {
+			formattedLine := prevLine
+			if options.count {
+				formattedLine = fmt.Sprintf("%d %v", counter, prevLine)
+			}
+			outputSlice = append(outputSlice, formattedLine)
+			prevLine = currentLine
+			counter = 1
+		} else {
+			counter++
 		}
 	}
+
+	formattedLine := prevLine
+	if options.count {
+		formattedLine = fmt.Sprintf("%d %v", counter, prevLine)
+	}
+
+	outputSlice = append(outputSlice, formattedLine)
+
 	return strings.Join(outputSlice, "\n")
 }
 
--- uniq/uniq_test.go
+++ uniq/uniq_test.go
@@ -20,3 +20,38 @@ func TestNoFlags(t *testing.T) {
 		require.Equalf(t, correctOut, out, "Strings don't match!\ntest string - '%v'", in)
 	}
 }
+
+func TestCount(t *testing.T) {
+	tests := map[string]string{
+		"hello world!":                             "1 hello world!",
+		"hello world!\nhello world!":               "2 hello world!",
+		"hello world!\nhello world!\nhi":           "2 hello world!\n1 hi",
+		"hi\nhello world!\nhello world!\nhi":       "1 hi\n2 hello world!\n1 hi",
+		"hi\n\nhello world!\nhello world!\n\n\nhi": "1 hi\n1 \n2 hello world!\n2 \n1 hi",
+	}
+
+	for in, correctOut := range tests {
+		out := uniq(parseString(in), Options{count: true})
+		require.Equalf(t, correctOut, out, "Strings don't match!\ntest string - '%v'", in)
+	}
+}
+
+// func TestCount(t *testing.T) {
+// 	tests := map[string]struct {
+// 		out		string
+// 		options	Options
+// 	} {
+// 		"hello world!\nhello world!\nhi": {
+// 			out: "hello world!",
+// 			options: Options{count: true},
+// 		},
+// 		"hello world!\nhello world!\nhi":           "hello world!\nhi",
+// 		"hi\nhello world!\nhello world!\nhi":       "hi\nhello world!\nhi",
+// 		"hi\n\nhello world!\nhello world!\n\n\nhi": "hi\n\nhello world!\n\nhi",
+// 	}
+
+// 	for in, correctOut := range tests {
+// 		out := uniq(parseString(in), Options{skip})
+// 		require.Equalf(t, correctOut, out, "Strings don't match!\ntest string - '%v'", in)
+// 	}
+// }
