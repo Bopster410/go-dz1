c15eb96cfd4f4257359d843a753b92478133d454
Bopster411 <serg.dr2004@gmail.com>
2024-02-18 11:26:56 +0300

    Expression struct

--- p2Calc/calc/calcCore.go
+++ p2Calc/calc/calcCore.go
@@ -5,31 +5,36 @@ import (
 	"strconv"
 )
 
+type Expr struct {
+	left   int
+	right  int
+	action rune
+}
+
 func CalcExpr(expr string) int {
-	var action string
-	var left, right int
+	var exprStruct Expr
 	// Regex for numerical expressions
 	re := regexp.MustCompile(`([1-9])\s*([+\-\/*])\s*([1-9])`)
 	for _, item := range re.FindStringSubmatch(expr)[1:] {
 		if item == "+" || item == "-" || item == "*" || item == "/" {
-			action = item
-		} else if action == "" {
-			left, _ = strconv.Atoi(item)
+			exprStruct.action = rune(item[0])
+		} else if exprStruct.action == '\x00' {
+			exprStruct.left, _ = strconv.Atoi(item)
 		} else {
-			right, _ = strconv.Atoi(item)
+			exprStruct.right, _ = strconv.Atoi(item)
 		}
 	}
 
 	var answer int
-	switch action {
-	case "+":
-		answer = left + right
-	case "-":
-		answer = left - right
-	case "*":
-		answer = left * right
-	case "/":
-		answer = left / right
+	switch exprStruct.action {
+	case '+':
+		answer = exprStruct.left + exprStruct.right
+	case '-':
+		answer = exprStruct.left - exprStruct.right
+	case '*':
+		answer = exprStruct.left * exprStruct.right
+	case '/':
+		answer = exprStruct.left / exprStruct.right
 	}
 
 	return answer
