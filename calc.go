7ba436baf44638ba4b303e86843a17d4dd39204d
Bopster411 <serg.dr2004@gmail.com>
2024-02-21 16:17:14 +0300

    Added input from cmd

--- calc.go
+++ calc.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/Bopster410/go-dz1/calc"
+)
+
+func main() {
+	exprStr := os.Args[1]
+	expr := calc.ParseExpr(exprStr)
+	fmt.Printf("%d", expr.CalcExpr())
 }
