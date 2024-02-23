29622e7805a095d4cf21c25b4038b7b77f67f207
Bopster411 <serg.dr2004@gmail.com>
2024-02-23 14:22:51 +0300

    Calc task

---
+++ calc/calcCore_test.go
@@ -0,0 +1,112 @@
+package calc
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSumBasic(t *testing.T) {
+	tests := map[string]int{
+		"1 + 1":     2,
+		"1 + 2":     3,
+		"1    +  2": 3,
+		"1+2":       3,
+	}
+	for in, out := range tests {
+		expr, err := ParseExpr(in)
+		require.Nil(t, err)
+		val, err := expr.CalcExpr()
+		require.Equal(t, out, val)
+		require.Nil(t, err)
+	}
+}
+
+func TestSubstractBasic(t *testing.T) {
+	tests := map[string]int{
+		"1 - 1":   0,
+		"2 - 1":   1,
+		"2-1":     1,
+		"2   - 1": 1,
+	}
+	for in, out := range tests {
+		expr, err := ParseExpr(in)
+		require.Nil(t, err)
+		val, err := expr.CalcExpr()
+		require.Equal(t, out, val)
+		require.Nil(t, err)
+	}
+}
+
+func TestMultiplyBasic(t *testing.T) {
+	tests := map[string]int{
+		"1 * 1":    1,
+		"2 * 3":    6,
+		"2*3":      6,
+		"2   *  3": 6,
+	}
+	for in, out := range tests {
+		expr, err := ParseExpr(in)
+		require.Nil(t, err)
+		val, err := expr.CalcExpr()
+		require.Equal(t, out, val)
+		require.Nil(t, err)
+	}
+}
+
+func TestDivisionBasic(t *testing.T) {
+	tests := map[string]int{
+		"1 / 1":    1,
+		"6 / 2":    3,
+		"6/2":      3,
+		"6   /  2": 3,
+	}
+	for in, out := range tests {
+		expr, err := ParseExpr(in)
+		require.Nil(t, err)
+		val, err := expr.CalcExpr()
+		require.Equal(t, out, val)
+		require.Nil(t, err)
+	}
+}
+
+func TestComplex(t *testing.T) {
+	tests := map[string]int{
+		"(1 + 1) * 2":     4,
+		"2 * (1 + 1)":     4,
+		"1 + 3 + 8 - 4":   8,
+		"1 + 3 + 8 * 4":   36,
+		"(1 + 3) * 8 - 4": 28,
+	}
+	for in, out := range tests {
+		expr, err := ParseExpr(in)
+		require.Nil(t, err)
+		val, err := expr.CalcExpr()
+		require.Equal(t, out, val)
+		require.Nil(t, err)
+	}
+}
+
+func TestWrongInput(t *testing.T) {
+	tests := map[string]error{
+		"(1 + 1 * 2": fmt.Errorf("wrong expression syntax"),
+		"2 * 1 + 1)": fmt.Errorf("wrong expression syntax"),
+		"aboba15":    fmt.Errorf("wrong expression syntax"),
+	}
+	for in, outError := range tests {
+		expr, err := ParseExpr(in)
+		require.Equal(t, outError, err)
+		require.Nil(t, expr)
+	}
+}
+
+func TestZeroDivision(t *testing.T) {
+	in := "1 / 0"
+	expr, parseErr := ParseExpr(in)
+	require.NotNil(t, expr)
+	require.Nil(t, parseErr)
+
+	_, calcErr := expr.CalcExpr()
+	require.Equal(t, fmt.Errorf("zero division occurred"), calcErr)
+}
